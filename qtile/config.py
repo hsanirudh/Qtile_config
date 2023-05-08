39cef7694cc4221a0fb31aeb8494d86d85cfc2b9
hsanirudh <hsanirudh@gmail.com>
2023-05-08 23:05:14 +0530

    Update to simple_key_binder() from libqtile.dgroups
    Signed-off-by: hsanirudh <hsanirudh@gmail.com>

--- qtile/config.py
+++ qtile/config.py
@@ -62,19 +62,19 @@ keys = [
 ]
 
 ## GroupBox ##
-group_names = [("", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
-]
 
-groups = [Group(name, **kwargs) for name, kwargs in group_names]
+groups = [Group("", layout='monadtall'),
+          Group("", layout='monadtall'),
+          Group("", layout='monadtall'),
+          Group("", layout='monadtall'),
+          Group("", layout='monadtall'),
+          Group("", layout='monadtall'),
+          Group("", layout='monadtall'),
+          Group("", layout='monadtall'),
+          Group("", layout='monadtall')]
 
-for i, (name, kwargs) in enumerate(group_names, 1):
-    keys.append(Key([mod], str(i), lazy.group[name].toscreen()))        # Switch to another group
-    keys.append(Key([mod, "shift"], str(i), lazy.window.togroup(name))) # Send current window to another group
+from libqtile.dgroups import simple_key_binder
+dgroups_key_binder = simple_key_binder(mod)
 
 ## Mouse_callback functions
 
